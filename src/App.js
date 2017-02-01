3f0f5c55a9576345ff5b5e3a455995f4a5901b4c
Brenton Cozby <brenton.cozby@gmail.com>
2017-02-01 10:24:40 -0500

    stateless component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import Landing from './landing/landing.main';
 import Footer from './footer/footer.main';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Markdown Previewer</h2>
-        </div>
-        <Landing />
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => 
+  <div className="App">
+    <div className="App-header">
+      <h2>Markdown Previewer</h2>
+    </div>
+    <Landing />
+    <Footer />
+  </div>;
 
 export default App;
