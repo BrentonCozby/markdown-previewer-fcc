6262a2226efcb4e545a40b66910a25089b675eb0
Brenton Cozby <brenton.cozby@gmail.com>
2017-02-01 10:26:08 -0500

    stateless component

--- src/footer/footer.main.js
+++ src/footer/footer.main.js
@@ -2,20 +2,15 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './footer.css';
 
-class Footer extends Component {
-    render() {
-        return (
-            <footer className='footer-main'>
-                <p className='created-by'>
-                    <img src={logo} className='react-logo' alt='React logo'/>
-                    Created by&nbsp;
-                    <a target='_blank' rel='noopener' href='https://brentoncozby.com'>Brenton Cozby</a>
-                    &nbsp;with&nbsp;
-                    <a target='_blank' rel='noopener' href='https://github.com/facebookincubator/create-react-app'>create-react-app</a>.
-                </p>
-            </footer>
-        );
-    }
-}
+const Footer = () => 
+    <footer className='footer-main'>
+        <p className='created-by'>
+            <img src={logo} className='react-logo' alt='React logo'/>
+            Created by&nbsp;
+            <a target='_blank' rel='noopener' href='https://brentoncozby.com'>Brenton Cozby</a>
+            &nbsp;with&nbsp;
+            <a target='_blank' rel='noopener' href='https://github.com/facebookincubator/create-react-app'>create-react-app</a>.
+        </p>
+    </footer>;
 
 export default Footer;
