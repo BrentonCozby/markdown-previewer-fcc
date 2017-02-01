e628e0e240473f3bc122b43c221c8b8d8a6173a1
Brenton Cozby <brenton.cozby@gmail.com>
2017-02-01 10:49:39 -0500

    stateless component

--- src/landing/landing.main.js
+++ src/landing/landing.main.js
@@ -2,14 +2,9 @@ import React, {Component} from 'react';
 import Previewer from './previewer/previewer.main';
 import './landing.css';
 
-class Landing extends Component {
-    render() {
-        return (
-            <div className='landing-main'>
-                <Previewer />
-            </div>
-        );
-    }
-}
+const Landing = () =>
+    <div className='landing-main'>
+        <Previewer />
+    </div>;
 
 export default Landing;
